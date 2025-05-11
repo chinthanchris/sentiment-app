e375a85f640344180d9c3d4d62b5d279df8ed859
Chinthan M <chinthanchris2004@gmail.com>
2025-05-11 13:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask, request, render_template
-from transformers import pipeline
 
-app = Flask(__name__)
-classifier = pipeline('sentiment-analysis')
-
-@app.route("/", methods=["GET", "POST"])
-def index():
-    sentiment = None
-    if request.method == "POST":
-        text = request.form["text"]
-        result = classifier(text)[0]
-        sentiment = f"{result['label']} ({result['score']:.2f})"
-    return render_template("index.html", sentiment=sentiment)
